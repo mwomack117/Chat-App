89b5bb7a674df90b228b26b71d86a1d6921dac43
mwomack <michael.womack84@gmail.com>
2019-09-06 21:42:58 -0700

    Added functions to add users, find user, remove users, and get users in a room

--- public/js/chat.js
+++ public/js/chat.js
@@ -11,6 +11,11 @@ const $messages = document.getElementById("messages");
 const messageTemplate = document.getElementById("message-template").innerHTML;
 const locationTemplate = document.getElementById("location-template").innerHTML;
 
+// Options
+const { username, room } = Qs.parse(location.search, {
+  ignoreQueryPrefix: true
+});
+
 socket.on("message", message => {
   console.log(message);
   const html = Mustache.render(messageTemplate, {
@@ -71,4 +76,11 @@ $sendLocationButton.addEventListener("click", () => {
       }
     );
   });
+});
+
+socket.emit("join", { username, room }, error => {
+  if (error) {
+    alert(error);
+    location.href = "/";
+  }
 });
--- src/index.js
+++ src/index.js
@@ -6,6 +6,13 @@ const {
   generateLocationMessage
 } = require("./utils/messages");
 
+const {
+  addUser,
+  removeUser,
+  getUser,
+  getUsersInRoom
+} = require("./utils/users");
+
 const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
@@ -16,8 +23,23 @@ app.use(express.static(path.join(__dirname, "../public")));
 
 io.on("connection", socket => {
   console.log("New Websocket connection");
-  socket.emit("message", generateMessage("Welcome!"));
-  socket.broadcast.emit("message", generateMessage("A new user has joined"));
+
+  socket.on("join", ({ username, room }, cb) => {
+    const { error, user } = addUser({ id: socket.id, username, room });
+
+    if (error) {
+      return cb(error);
+    }
+
+    socket.join(user.room);
+
+    socket.emit("message", generateMessage("Welcome!"));
+    socket.broadcast
+      .to(user.room)
+      .emit("message", generateMessage(`${user.username} has joined!`));
+
+    cb();
+  });
 
   socket.on("sendMessage", (message, cb) => {
     const filter = new Filter();
@@ -26,7 +48,7 @@ io.on("connection", socket => {
       return cb("Profanity is not allowed");
     }
 
-    io.emit("message", generateMessage(message));
+    io.to("climbing").emit("message", generateMessage(message));
     cb();
   });
 
@@ -41,7 +63,11 @@ io.on("connection", socket => {
   });
 
   socket.on("disconnect", () => {
-    io.emit("message", generateMessage("A user has left"));
+    const user = removeUser(socket.id);
+
+    if (user) {
+      io.to.emit("message", generateMessage(`${user} has left!`));
+    }
   });
 });
 
